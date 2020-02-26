748c4496b8dd3698e08f51f9333b267256583138
Cameron Holdaway <cameron.holdaway@gmail.com>
2020-02-25 21:13:25 -0800

    added colors, removed race ID

---
+++ PE/BuildJSON.js
@@ -0,0 +1,610 @@
+var myData = [
+  {
+    "asian": 96,
+    "white": 91,
+    "black": 22,
+    "hispanic": 36
+  },
+  {
+    "asian": 96,
+    "white": 91,
+    "black": 22,
+    "hispanic": 36
+  },
+  {
+    "asian": 96,
+    "white": 90,
+    "black": 22,
+    "hispanic": 36
+  },
+  {
+    "asian": 96,
+    "white": 90,
+    "black": 22,
+    "hispanic": 37
+  },
+  {
+    "asian": 96,
+    "white": 90,
+    "black": 23,
+    "hispanic": 37
+  },
+  {
+    "asian": 96,
+    "white": 90,
+    "black": 23,
+    "hispanic": 37
+  },
+  {
+    "asian": 96,
+    "white": 90,
+    "black": 23,
+    "hispanic": 37
+  },
+  {
+    "asian": 95,
+    "white": 89,
+    "black": 23,
+    "hispanic": 37
+  },
+  {
+    "asian": 95,
+    "white": 89,
+    "black": 24,
+    "hispanic": 37
+  },
+  {
+    "asian": 95,
+    "white": 89,
+    "black": 24,
+    "hispanic": 37
+  },
+  {
+    "asian": 95,
+    "white": 89,
+    "black": 24,
+    "hispanic": 38
+  },
+  {
+    "asian": 95,
+    "white": 88,
+    "black": 24,
+    "hispanic": 38
+  },
+  {
+    "asian": 94,
+    "white": 88,
+    "black": 25,
+    "hispanic": 38
+  },
+  {
+    "asian": 94,
+    "white": 88,
+    "black": 25,
+    "hispanic": 38
+  },
+  {
+    "asian": 94,
+    "white": 87,
+    "black": 25,
+    "hispanic": 38
+  },
+  {
+    "asian": 94,
+    "white": 87,
+    "black": 25,
+    "hispanic": 38
+  },
+  {
+    "asian": 94,
+    "white": 87,
+    "black": 26,
+    "hispanic": 38
+  },
+  {
+    "asian": 93,
+    "white": 87,
+    "black": 26,
+    "hispanic": 39
+  },
+  {
+    "asian": 93,
+    "white": 86,
+    "black": 26,
+    "hispanic": 39
+  },
+  {
+    "asian": 93,
+    "white": 86,
+    "black": 26,
+    "hispanic": 39
+  },
+  {
+    "asian": 93,
+    "white": 86,
+    "black": 27,
+    "hispanic": 39
+  },
+  {
+    "asian": 92,
+    "white": 85,
+    "black": 27,
+    "hispanic": 39
+  },
+  {
+    "asian": 92,
+    "white": 85,
+    "black": 27,
+    "hispanic": 39
+  },
+  {
+    "asian": 92,
+    "white": 85,
+    "black": 27,
+    "hispanic": 39
+  },
+  {
+    "asian": 92,
+    "white": 85,
+    "black": 28,
+    "hispanic": 39
+  },
+  {
+    "asian": 91,
+    "white": 84,
+    "black": 28,
+    "hispanic": 40
+  },
+  {
+    "asian": 91,
+    "white": 84,
+    "black": 28,
+    "hispanic": 40
+  },
+  {
+    "asian": 91,
+    "white": 84,
+    "black": 28,
+    "hispanic": 40
+  },
+  {
+    "asian": 90,
+    "white": 83,
+    "black": 29,
+    "hispanic": 40
+  },
+  {
+    "asian": 90,
+    "white": 83,
+    "black": 29,
+    "hispanic": 40
+  },
+  {
+    "asian": 90,
+    "white": 83,
+    "black": 29,
+    "hispanic": 40
+  },
+  {
+    "asian": 89,
+    "white": 82,
+    "black": 29,
+    "hispanic": 40
+  },
+  {
+    "asian": 89,
+    "white": 82,
+    "black": 30,
+    "hispanic": 40
+  },
+  {
+    "asian": 89,
+    "white": 81,
+    "black": 30,
+    "hispanic": 41
+  },
+  {
+    "asian": 88,
+    "white": 81,
+    "black": 30,
+    "hispanic": 41
+  },
+  {
+    "asian": 88,
+    "white": 81,
+    "black": 30,
+    "hispanic": 41
+  },
+  {
+    "asian": 88,
+    "white": 80,
+    "black": 31,
+    "hispanic": 41
+  },
+  {
+    "asian": 87,
+    "white": 80,
+    "black": 31,
+    "hispanic": 41
+  },
+  {
+    "asian": 87,
+    "white": 79,
+    "black": 31,
+    "hispanic": 41
+  },
+  {
+    "asian": 86,
+    "white": 79,
+    "black": 31,
+    "hispanic": 41
+  },
+  {
+    "asian": 86,
+    "white": 79,
+    "black": 32,
+    "hispanic": 41
+  },
+  {
+    "asian": 86,
+    "white": 78,
+    "black": 32,
+    "hispanic": 41
+  },
+  {
+    "asian": 85,
+    "white": 78,
+    "black": 32,
+    "hispanic": 42
+  },
+  {
+    "asian": 85,
+    "white": 77,
+    "black": 32,
+    "hispanic": 42
+  },
+  {
+    "asian": 84,
+    "white": 77,
+    "black": 33,
+    "hispanic": 42
+  },
+  {
+    "asian": 84,
+    "white": 77,
+    "black": 33,
+    "hispanic": 42
+  },
+  {
+    "asian": 83,
+    "white": 76,
+    "black": 33,
+    "hispanic": 42
+  },
+  {
+    "asian": 83,
+    "white": 76,
+    "black": 33,
+    "hispanic": 42
+  },
+  {
+    "asian": 82,
+    "white": 75,
+    "black": 34,
+    "hispanic": 42
+  },
+  {
+    "asian": 82,
+    "white": 75,
+    "black": 34,
+    "hispanic": 42
+  },
+  {
+    "asian": 81,
+    "white": 74,
+    "black": 34,
+    "hispanic": 42
+  },
+  {
+    "asian": 81,
+    "white": 74,
+    "black": 34,
+    "hispanic": 42
+  },
+  {
+    "asian": 80,
+    "white": 73,
+    "black": 35,
+    "hispanic": 42
+  },
+  {
+    "asian": 80,
+    "white": 73,
+    "black": 35,
+    "hispanic": 43
+  },
+  {
+    "asian": 79,
+    "white": 72,
+    "black": 35,
+    "hispanic": 43
+  },
+  {
+    "asian": 79,
+    "white": 72,
+    "black": 35,
+    "hispanic": 43
+  },
+  {
+    "asian": 78,
+    "white": 71,
+    "black": 36,
+    "hispanic": 43
+  },
+  {
+    "asian": 77,
+    "white": 71,
+    "black": 36,
+    "hispanic": 43
+  },
+  {
+    "asian": 77,
+    "white": 70,
+    "black": 36,
+    "hispanic": 43
+  },
+  {
+    "asian": 76,
+    "white": 70,
+    "black": 36,
+    "hispanic": 43
+  },
+  {
+    "asian": 75,
+    "white": 69,
+    "black": 36,
+    "hispanic": 43
+  },
+  {
+    "asian": 75,
+    "white": 69,
+    "black": 37,
+    "hispanic": 43
+  },
+  {
+    "asian": 74,
+    "white": 68,
+    "black": 37,
+    "hispanic": 43
+  },
+  {
+    "asian": 74,
+    "white": 68,
+    "black": 37,
+    "hispanic": 43
+  },
+  {
+    "asian": 73,
+    "white": 67,
+    "black": 37,
+    "hispanic": 43
+  },
+  {
+    "asian": 72,
+    "white": 66,
+    "black": 37,
+    "hispanic": 43
+  },
+  {
+    "asian": 72,
+    "white": 66,
+    "black": 38,
+    "hispanic": 43
+  },
+  {
+    "asian": 71,
+    "white": 65,
+    "black": 38,
+    "hispanic": 43
+  },
+  {
+    "asian": 70,
+    "white": 65,
+    "black": 38,
+    "hispanic": 43
+  },
+  {
+    "asian": 69,
+    "white": 64,
+    "black": 38,
+    "hispanic": 43
+  },
+  {
+    "asian": 69,
+    "white": 63,
+    "black": 38,
+    "hispanic": 43
+  },
+  {
+    "asian": 68,
+    "white": 63,
+    "black": 39,
+    "hispanic": 43
+  },
+  {
+    "asian": 67,
+    "white": 62,
+    "black": 39,
+    "hispanic": 44
+  },
+  {
+    "asian": 66,
+    "white": 62,
+    "black": 39,
+    "hispanic": 44
+  },
+  {
+    "asian": 66,
+    "white": 61,
+    "black": 39,
+    "hispanic": 44
+  },
+  {
+    "asian": 65,
+    "white": 60,
+    "black": 39,
+    "hispanic": 44
+  },
+  {
+    "asian": 64,
+    "white": 60,
+    "black": 40,
+    "hispanic": 44
+  },
+  {
+    "asian": 63,
+    "white": 59,
+    "black": 40,
+    "hispanic": 44
+  },
+  {
+    "asian": 62,
+    "white": 58,
+    "black": 40,
+    "hispanic": 44
+  },
+  {
+    "asian": 62,
+    "white": 58,
+    "black": 40,
+    "hispanic": 44
+  },
+  {
+    "asian": 61,
+    "white": 57,
+    "black": 40,
+    "hispanic": 44
+  },
+  {
+    "asian": 60,
+    "white": 56,
+    "black": 40,
+    "hispanic": 44
+  },
+  {
+    "asian": 59,
+    "white": 56,
+    "black": 41,
+    "hispanic": 44
+  },
+  {
+    "asian": 58,
+    "white": 55,
+    "black": 41,
+    "hispanic": 44
+  },
+  {
+    "asian": 57,
+    "white": 54,
+    "black": 41,
+    "hispanic": 44
+  },
+  {
+    "asian": 57,
+    "white": 54,
+    "black": 41,
+    "hispanic": 44
+  },
+  {
+    "asian": 56,
+    "white": 53,
+    "black": 41,
+    "hispanic": 44
+  },
+  {
+    "asian": 55,
+    "white": 52,
+    "black": 41,
+    "hispanic": 44
+  },
+  {
+    "asian": 54,
+    "white": 52,
+    "black": 42,
+    "hispanic": 44
+  },
+  {
+    "asian": 53,
+    "white": 51,
+    "black": 42,
+    "hispanic": 44
+  },
+  {
+    "asian": 52,
+    "white": 50,
+    "black": 42,
+    "hispanic": 44
+  },
+  {
+    "asian": 51,
+    "white": 50,
+    "black": 42,
+    "hispanic": 44
+  },
+  {
+    "asian": 50,
+    "white": 49,
+    "black": 42,
+    "hispanic": 44
+  },
+  {
+    "asian": 50,
+    "white": 48,
+    "black": 42,
+    "hispanic": 44
+  },
+  {
+    "asian": 49,
+    "white": 48,
+    "black": 42,
+    "hispanic": 43
+  },
+  {
+    "asian": 48,
+    "white": 47,
+    "black": 43,
+    "hispanic": 43
+  },
+  {
+    "asian": 47,
+    "white": 46,
+    "black": 43,
+    "hispanic": 43
+  },
+  {
+    "asian": 46,
+    "white": 45,
+    "black": 43,
+    "hispanic": 43
+  },
+  {
+    "asian": 45,
+    "white": 45,
+    "black": 43,
+    "hispanic": 43
+  },
+  {
+    "asian": 44,
+    "white": 44,
+    "black": 43,
+    "hispanic": 43
+  },
+  {
+    "asian": 43,
+    "white": 43,
+    "black": 43,
+    "hispanic": 43
+  }
+]
+  
+

52207153f1b2c3a419d92b6b77d5f6c0231f8b85
Cameron Holdaway <cameron.holdaway@gmail.com>
2020-04-12 21:34:06 -0600

    pilot analysis

--- JSONAnalysis.R
+++ JSONAnalysis.R
@@ -1,7 +1,8 @@
 library(jsonlite)
 library(tidyverse)
-json.data = fromJSON("./data/sona-34902.json")
+#json.data = fromJSON("./data/sona-34902.json")
 
+#Read in and merge all json data files
 setwd("./data")
 file_list <- list.files()
 for (file in file_list){
@@ -47,35 +48,60 @@ df$prompt.type <- ifelse(grepl("loan", df$prompt, ignore.case = T), "loan",
                                 ifelse(grepl("bail", df$prompt, ignore.case = T), "bail",
                                        ifelse(grepl("job", df$prompt, ignore.case = T), "job",
                                               ifelse(grepl("meals", df$prompt, ignore.case = T), "meals",
-                                                     ifelse(grepl("respirator", df$prompt, ignore.case = T), "respirator","Other"))))))
+                                                    ifelse(grepl("respirator", df$prompt, ignore.case = T), "respirator","Other"))))))
+##remove attention trials
 df = df %>% 
   filter(att == 0)
 
-#df$dataType = df$chosenData["type"][,1]
-#df$mu = df$chosenData["mu"][,1]
-
-
-ggplot(df, aes(y = response, x = dataType)) +
-  geom_boxplot()+ facet_grid(.~prompt.type)
-
-
-
+#plots of response by datatype and prompt
 library(ggpubr)
-ggerrorplot(df, x = "prompt.type", y = "response", 
-            desc_stat = "mean_se")
+ggerrorplot(df, x = "prompt.type", y = "mu", 
+            desc_stat = "mean_se")+
+  ggtitle("Mu Distribution by Prompt")
 
 df %>% mutate(name = fct_relevel(dataType, 
                             "LOW(-50,150)", 
                             "MED(-40,160)", 
                             "HIGH(-30,170)")) %>% 
-  ggerrorplot(x = "prompt.type", y = "mu", 
+  ggerrorplot(x = "prompt.type", y = "response", 
             desc_stat = "mean_se") + 
   facet_grid(.~name)+
-  theme(axis.text.x = element_text(angle = 90))
+  theme(axis.text.x = element_text(angle = 90))+
+  ggtitle("Response by Prompt and Tradeoff")
 
-
+#Distribution of responses
 ggplot(df, aes(response)) +
   geom_histogram(bins = 15) +facet_grid(.~dataType)
-
-
 table(df$mu, df$dataType)
+
+
+##Create DF to compare correlation between trials 
+df_first_obs = df %>% 
+  arrange(trialNumber) %>% 
+  group_by(id, prompt.type, dataType) %>% 
+  slice(1) %>% 
+  select(id, dataType, prompt.type, mu, response)
+df_second_obs = df %>% arrange(trialNumber) %>% 
+  group_by(id, prompt.type, dataType) %>% slice(n()) %>% 
+  select(id, dataType, prompt.type, mu, response)
+df_grouped = left_join(df_first_obs, df_second_obs, by=c("id","dataType", "prompt.type")) %>% rowwise()
+
+#create equation for correlation
+corr_eqn = function(x,y, digits = 2) {
+  corr_coef <- round(cor(x, y), digits = digits)
+  paste("italic(r) == ", corr_coef)
+}
+
+labels = data.frame(x = 25, y = 55, label = corr_eqn(df_grouped$mu.x, df_grouped$mu.y))
+
+ggplot(df_grouped, aes(x = mu.x, y = mu.y))+
+  geom_point()+
+  ggtitle("Correlation of Mu (repeated trials) within subject")+
+  xlab("Mu (first trial)") +
+  ylab("Mu (second trial)")+
+  geom_smooth(method = "lm")+
+  geom_text(data = labels, aes(x = 25, y = 55, label = label), parse = TRUE)
+
+
+
+
